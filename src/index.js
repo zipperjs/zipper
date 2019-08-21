7abad35cecfad05861b87fd47fe59c88ae87a462
Gilberto Albino <contato@gilbertoalbino.com.br>
2019-08-20 23:09:07 -0300

    Adiciona instruções de contribuição

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@
  * é um CMS alternativo
  * para gestão de conteúdo na internet.
  * 
+ * Este projeto é OpenSource, 
+ * se deseja contribuir visite
+ * https://zipper.github.io
+ * para mais informações.
+ * 
  * @author Gilberto Albino
  * @since 2019-08-20
  */
